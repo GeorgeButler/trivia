b4b4010239c24fc3dfd2a37acf7cc33e83a9ed4e
George Butler <georgebutleriv@gmail.com>
2019-05-02 19:21:47 -0700

    Minor changes

--- components/Question.vue
+++ components/Question.vue
@@ -1,49 +1,53 @@
 <template>
-	<section class="section">
-		<h1 class="title has-text-centered" v-html="question"></h1>
-		<div class="buttons">
-			<button
-				class="button is-primary is-large is-fullwidth"
-				v-for="answer in answers"
-				v-bind:key="answer"
-				v-on:click="clickAnswer"
-			>{{ answer }}</button>
-		</div>
-	</section>
+  <section class="section">
+    <h1 class="title has-text-centered" v-html="question"></h1>
+    <div class="buttons">
+      <button
+        class="button is-primary is-large is-fullwidth"
+        v-for="answer in answers"
+        v-bind:key="answer"
+        v-on:click="clickAnswer(answer, correct_answer)"
+        v-html="answer"
+      ></button>
+    </div>
+  </section>
 </template>
 
 <script>
 import _ from 'lodash'
 
 export default {
-	name: 'Question',
-	props: {
-		question: {
-			type: String,
-			required: true
-		},
-		correct_answer: {
-			type: String,
-			required: true
-		},
-		incorrect_answers: {
-			type: Array,
-			required: true
-		}
-	},
-	computed: {
-		answers: function() {
-			let arr = _.clone(this.incorrect_answers)
-			arr.push(this.correct_answer)
-			_.shuffle(arr)
+  name: 'Question',
+  props: {
+    question: {
+      type: String,
+      required: true
+    },
+    correct_answer: {
+      type: String,
+      required: true
+    },
+    incorrect_answers: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    answers: function() {
+      let arr = _.clone(this.incorrect_answers)
+      arr.push(this.correct_answer)
+      _.shuffle(arr)
 
-			return arr
-		}
-	},
-	methods: {
-		clickAnswer: function() {
-			this.$store.dispatch('setAnswer')
-		}
-	}
+      return arr
+    }
+  },
+  methods: {
+    clickAnswer: function(text, correctText) {
+      this.$store.dispatch('setAnswer', {
+        answer: text,
+        correct: correctText
+      })
+    }
+  }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,31 +1,43 @@
 export const state = () => ({
-	response: {},
-	game: {
-		currentQuestion: 0,
-		currentScore: 0,
-		currentAnswer: ''
-	}
+  response: {},
+  game: {
+    currentQuestion: 0,
+    currentScore: 0,
+    currentAnswer: ''
+  }
 })
 
 export const mutations = {
-	setResponse (state, response) {
-		state.response = response
-	},
-	setAnswer (state, answer) {
-		state.game.currentAnswer = answer
-		// Advance question
-	}
+  setResponse(state, response) {
+    state.response = response
+  },
+  advanceQuestion(state) {
+    state.game.currentQuestion = state.game.currentQuestion + 1
+  },
+  setAnswer(state, answer, correct) {
+    state.game.currentAnswer = answer
+
+    console.log(correct)
+    if (answer === correct) {
+      console.log("NICE!")
+    }
+  }
 }
 
 export const actions = {
-	async getResponse ({ commit }) {
-		await this.$axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
-			if (res.status === 200) {
-				commit('setResponse', res.data)
-			}
-		})
-	},
-	async setAnswer ({ commit }) {
-		commit('setAnswer', 'YEET')
-	}
+  async getResponse({
+    commit
+  }) {
+    await this.$axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
+      if (res.status === 200) {
+        commit('setResponse', res.data)
+      }
+    })
+  },
+  async setAnswer({
+    commit
+  }, payload) {
+    commit('setAnswer', payload.answer, payload.correct)
+    //commit('advanceQuestion')
+  }
 }
