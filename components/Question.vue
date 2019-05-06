34b67f832d5d59c18918a63f0e422487de1cb92a
George <georgebutleriv@gmail.com>
2019-05-05 23:51:06 -0700

    Minor changes

--- components/Question.vue
+++ components/Question.vue
@@ -1,53 +1,57 @@
 <template>
-  <section class="section">
-    <h1 class="title has-text-centered" v-html="question"></h1>
-    <div class="buttons">
-      <button
-        class="button is-primary is-large is-fullwidth"
-        v-for="answer in answers"
-        v-bind:key="answer"
-        v-on:click="clickAnswer(answer, correct_answer)"
-        v-html="answer"
-      ></button>
-    </div>
-  </section>
+	<section class="section">
+		<h1 class="title has-text-centered" v-html="question"></h1>
+		<div class="buttons">
+			<button
+				class="button is-primary is-large is-fullwidth"
+				v-for="answer in answers"
+				v-bind:key="answer"
+				v-on:click="clickAnswer(answer, correct_answer)"
+				v-html="answer"
+			></button>
+		</div>
+	</section>
 </template>
 
 <script>
 import _ from 'lodash'
 
 export default {
-  name: 'Question',
-  props: {
-    question: {
-      type: String,
-      required: true
-    },
-    correct_answer: {
-      type: String,
-      required: true
-    },
-    incorrect_answers: {
-      type: Array,
-      required: true
-    }
-  },
-  computed: {
-    answers: function() {
-      let arr = _.clone(this.incorrect_answers)
-      arr.push(this.correct_answer)
-      _.shuffle(arr)
+	name: 'Question',
+	props: {
+		question: {
+			type: String,
+			required: true
+		},
+		correct_answer: {
+			type: String,
+			required: true
+		},
+		incorrect_answers: {
+			type: Array,
+			required: true
+		},
+		type: {
+			type: String,
+			required: true
+		}
+	},
+	computed: {
+		answers: function() {
+			let arr = _.clone(this.incorrect_answers)
+			arr.push(this.correct_answer)
+			_.shuffle(arr)
 
-      return arr
-    }
-  },
-  methods: {
-    clickAnswer: function(text, correctText) {
-      this.$store.dispatch('setAnswer', {
-        answer: text,
-        correct: correctText
-      })
-    }
-  }
+			return arr
+		}
+	},
+	methods: {
+		clickAnswer: function(text, correctText) {
+			this.$store.dispatch('setAnswer', {
+				answer: text,
+				correct: correctText
+			})
+		}
+	}
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="container">
-    <section class="section is-unselectable">
-      <div class="columns">
-        <div class="column has-text-centered">{{ game.currentQuestion + 1 }} / {{ totalQuestions }}</div>
-        <div class="column has-text-centered">
-          <div>{{ currentQuestion.category }}</div>
-          <div>{{ currentQuestion.difficulty }}</div>
-        </div>
-      </div>
-      <Question v-bind="currentQuestion"></Question>
-    </section>
-  </div>
+	<div class="container">
+		<section class="section is-unselectable">
+			<div class="columns">
+				<div class="column has-text-centered">{{ game.currentQuestion + 1 }} / {{ totalQuestions }}</div>
+				<div class="column has-text-centered">
+					<div class="is-capitalized">{{ currentQuestion.category }}</div>
+					<div class="is-capitalized">{{ currentQuestion.difficulty }}</div>
+				</div>
+			</div>
+			<Question v-bind="currentQuestion"></Question>
+			<div class="columns">
+				<div class="column has-text-centered">
+					<div class="is-capitalized">Score: {{ game.currentScore }}</div>
+				</div>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
@@ -18,25 +23,25 @@ import { mapState } from 'vuex'
 import Question from '../components/Question'
 
 export default {
-  name: 'Home',
-  components: {
-    Question
-  },
-  computed: {
-    ...mapState({
-      currentQuestion: state => {
-        return state.response.results[state.game.currentQuestion]
-      },
-      game: state => {
-        return state.game
-      },
-      totalQuestions: state => {
-        return state.response.results.length
-      }
-    })
-  },
-  async fetch({ store }) {
-    await store.dispatch('getResponse')
-  }
+	name: 'Home',
+	components: {
+		Question
+	},
+	computed: {
+		...mapState({
+			currentQuestion: state => {
+				return state.response.results[state.game.currentQuestion]
+			},
+			game: state => {
+				return state.game
+			},
+			totalQuestions: state => {
+				return state.response.results.length
+			}
+		})
+	},
+	async fetch({ store }) {
+		await store.dispatch('getResponse')
+	}
 }
 </script>
